92ff4d2e5f4f3e38eafc1a66b7aab2dce2bbc857
emilyyleung <emily_y_leung@outlook.com>
2022-09-28 21:13:09 +1000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: '/utility',
+  assetPrefix: '/utility',
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
