50b265f28b37c1c728740699eeb9728811baa61e
emilyyleung <emily_y_leung@outlook.com>
2022-09-30 23:10:37 +1000

    setup copy to clipboard button

--- components/Form/Input/NumberInput.js
+++ components/Form/Input/NumberInput.js
@@ -7,41 +7,73 @@ export default function NumberInput({
   step,
 }) {
   return (
-    <label className="block">
-      <span className="block text-sm font-medium text-gray-700 pb-2">
-        {title}
-      </span>
-      <div className="grid grid-cols-[1fr_auto] place-items-center gap-2">
-        <input
-          type="number"
-          min={min}
-          max={max}
-          step={step}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onKeyDown={(e) => {
-            e.code === 'ArrowUp' || e.code === 'ArrowDown'
-              ? setValue(e.target.value)
-              : e.preventDefault();
-          }}
-          className="rounded-md border-gray-300 shadow-sm form-input mt-1 block w-full"
-        />
+    <div className="w-full">
+      <label className="block">
+        {/* <span className="block text-sm font-medium text-gray-700 pb-2">
+          {title}
+        </span> */}
+        <div className="grid grid-cols-[auto_1fr_auto] place-items-center gap-2">
+          <button
+            onClick={() => {
+              if (parseInt(value) < max) {
+                setValue(parseInt(value) + 5);
+              }
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
+              />
+            </svg>
+          </button>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-6 h-6 text-slate-400"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+          <input
+            type="number"
+            min={min}
+            max={max}
+            step={step}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={(e) => {
+              e.code === 'ArrowUp' || e.code === 'ArrowDown'
+                ? setValue(e.target.value)
+                : e.preventDefault();
+            }}
+            className="rounded-md border-gray-300 shadow-sm form-input mt-1 block w-full"
           />
-        </svg>
-      </div>
-    </label>
+          <button
+            onClick={() => {
+              if (parseInt(value) > min) {
+                setValue(parseInt(value) - 5);
+              }
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+              />
+            </svg>
+          </button>
+        </div>
+      </label>
+    </div>
   );
 }
--- pages/commit.js
+++ pages/commit.js
@@ -3,8 +3,11 @@ import { useState, useEffect } from 'react';
 import TextArea from 'components/Form/Input/TextArea';
 import NumberInput from 'components/Form/Input/NumberInput';
 
+const loremIpsum =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in nisi at dui faucibus tincidunt eu sit amet libero. Quisque lacinia est non dolor faucibus tempus. Vivamus dictum mattis nibh, vitae tristique tellus consectetur in. Cras vitae dignissim diam. Nullam vel quam dolor. Proin non metus elementum, porttitor nisl nec, tempus felis. Suspendisse in scelerisque magna. Donec massa lorem, luctus at viverra id, vulputate vel elit. Sed cursus, nulla ac gravida consectetur, lorem augue dignissim nulla, vitae accumsan justo enim posuere odio. Nulla facilisi. Phasellus tincidunt a neque a viverra. Nam rhoncus eu nunc id viverra. Sed consectetur vulputate erat ut ornare. Etiam molestie efficitur tincidunt.';
+
 export default function CommitPage() {
-  const [commitMessage, setCommitMessage] = useState('');
+  const [commitMessage, setCommitMessage] = useState(loremIpsum);
 
   const [output, setOutput] = useState('');
 
@@ -49,17 +52,7 @@ export default function CommitPage() {
 
   return (
     <div className="bg-slate-200 min-h-screen">
-      <div className="mx-auto pt-10 max-w-3xl px-10 lg:px-0 ">
-        <div className="pb-10 w-52">
-          <NumberInput
-            title="Max message width"
-            value={rowWidth}
-            setValue={setRowWidth}
-            min={30}
-            max={100}
-            step={5}
-          />
-        </div>
+      <div className="mx-auto pt-10 max-w-3xl px-10 lg:px-0 grid gap-4">
         <TextArea
           label="Write your commit message"
           name="commit"
@@ -67,8 +60,45 @@ export default function CommitPage() {
           setValue={setCommitMessage}
           value={commitMessage}
         />
+        <div className="grid sm:grid-cols-[auto_200px_1fr] grid-cols-[auto_1fr_auto] gap-4 place-items-center">
+          <div className="text-sm font-medium text-gray-700">
+            Character Width
+          </div>
+          <div className="w-full">
+            <NumberInput
+              title="Character width"
+              value={rowWidth}
+              setValue={setRowWidth}
+              min={30}
+              max={100}
+              step={5}
+            />
+          </div>
+          <div className="justify-self-end">
+            {/* <button
+              className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              type="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                />
+              </svg>
+              <div>Copy</div>
+            </button> */}
+          </div>
+        </div>
         <p
-          className="pt-10 overflow-x-auto custom-scroll"
+          className="overflow-x-auto custom-scroll"
           style={{ whiteSpace: 'pre' }}
         >
           {output}
